cf3e3f3e2a521d503f18212bac79768fe7b85181
LevisLopez <96857417+LevisLopez@users.noreply.github.com>
2026-06-02 16:50:47 -0500

    Update cache name from 'aurora-v18' to 'aurora-v19'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aurora-v18';
+const CACHE_NAME = 'aurora-v19';
 const APP_SHELL = [
   './', './index.html', './style.css',
   './db.js', './player.js', './app.js', './sleep.js', './lyrics.js',
