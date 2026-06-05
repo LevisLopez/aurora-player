2812ba98e08a96bf4b46a0d7289015be0c3133d0
LevisLopez <96857417+LevisLopez@users.noreply.github.com>
2026-06-05 15:40:56 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aurora-v42';
+const CACHE_NAME = 'aurora-v43';
 const APP_SHELL = [
   './', './index.html', './style.css',
   './db.js', './player.js', './app.js', './sleep.js',
