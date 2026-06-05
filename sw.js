4c47d1d5b5c6266d1bc9372e938149975aee7a0f
LevisLopez <96857417+LevisLopez@users.noreply.github.com>
2026-06-05 16:44:27 -0500

    Update CACHE_NAME to 'aurora-v34'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aurora-v45';
+const CACHE_NAME = 'aurora-v34';
 const APP_SHELL = [
   './', './index.html', './style.css',
   './db.js', './player.js', './app.js', './sleep.js',
